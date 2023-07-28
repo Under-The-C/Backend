a2bac9d3abd5ba8e48552953044002d5690c47b5
feedbaek <nicek789@naver.com>
2023-07-28 19:43:26 +0900

    Init Project

---
+++ src/main/java/LikeLion/UnderTheCBackend/UnderTheCBackendApplication.java
@@ -0,0 +1,13 @@
+package LikeLion.UnderTheCBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderTheCBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderTheCBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LikeLion/UnderTheCBackend/UnderTheCBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package LikeLion.UnderTheCBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderTheCBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
