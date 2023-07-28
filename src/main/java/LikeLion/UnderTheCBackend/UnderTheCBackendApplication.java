283dade534221f024b6936570c405b1fbadec02b
feedbaek <nicek789@naver.com>
2023-07-28 21:49:24 +0900

    Feat: application.properties 설정

--- src/main/java/LikeLion/UnderTheCBackend/UnderTheCBackendApplication.java
+++ src/main/java/LikeLion/UnderTheCBackend/UnderTheCBackendApplication.java
@@ -1,13 +1,72 @@
 package LikeLion.UnderTheCBackend;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UnderTheCBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnderTheCBackendApplication.class, args);
+	}
+
+	/* Cors 설정 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://" + System.getenv("REACT_HOST") + ":" + System.getenv("REACT_PORT"), "http://localhost:" + System.getenv("REACT_PORT"))
+						.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+						.allowCredentials(true);
+			}
+		};
+	}
+
+	/* HTTPS 설정 */
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// Enable SSL Trafic
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		// Add HTTP to HTTPS redirect
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+		return tomcat;
+	}
+
+	/*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setPort(80);
+		connector.setScheme("http");
+		connector.setSecure(false);
+		connector.setRedirectPort(443);
+		return connector;
 	}
 
 }
