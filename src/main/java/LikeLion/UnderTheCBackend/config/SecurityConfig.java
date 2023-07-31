a6a45ed6e917a14742fcf36045830d0d82d07da9
feedbaek <nicek789@naver.com>
2023-08-01 00:42:20 +0900

    Feat: spring-security 적용해서 허용 url외 전부 로그인 필요

---
+++ src/main/java/LikeLion/UnderTheCBackend/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package LikeLion.UnderTheCBackend.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] WHITE_LIST = {
+            "/",
+    };
+
+    @Bean
+    protected SecurityFilterChain myConfig(HttpSecurity http) throws Exception {
+        /* 허용 페이지 등록 */
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(WHITE_LIST).permitAll());
+        /* 로그인 페이지 설정 */
+        http
+                .formLogin(form -> form
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .loginProcessingUrl("/login_proc")
+                        .successHandler(
+                                new AuthenticationSuccessHandler() {
+                                    @Override
+                                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                        System.out.println("authentication : " + authentication.getName());
+                                        response.sendRedirect("/"); // 인증이 성공한 후에는 무조건 root로 이동시킴
+                                    }
+                                }
+                        )
+                        .failureHandler(
+                                new AuthenticationFailureHandler() {
+                                    @Override
+                                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                        System.out.println("exception : " + exception.getMessage());
+                                        response.sendRedirect("/login");
+                                    }
+                                }
+                        )
+                        .permitAll()
+
+                );
+        return http.build();
+    }
+}
