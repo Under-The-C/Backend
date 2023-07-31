7f1be49f2ec1e4c0ab6ed7f35ddb6cfd61735328
feedbaek <nicek789@naver.com>
2023-07-31 22:40:00 +0900

    Feat: 로컬, 서버 properties 분할

--- fastbuild.sh
+++ fastbuild.sh
@@ -4,4 +4,4 @@ git pull
 git checkout develop
 
 ./gradlew build
-java -jar build/libs/UnderTheC-Backend-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=prod build/libs/UnderTheC-Backend-0.0.1-SNAPSHOT.jar
