378e52c92679a465b6de4ea96651d6a34cc3fffd
feedbaek <nicek789@naver.com>
2023-08-03 21:57:08 +0900

    Fix: API 경로 수정

--- src/main/java/LikeLion/UnderTheCBackend/config/SecurityConfig.java
+++ src/main/java/LikeLion/UnderTheCBackend/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/swagger-resources/**",
-            "/v1/api/**"
+            "/api/v1/**"
     };
 
     @Bean
