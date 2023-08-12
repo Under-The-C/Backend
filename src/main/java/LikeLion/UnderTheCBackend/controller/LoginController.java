7e3834aeaeea3c8ddf95b531855eb4eb5eaba604
naminlee <nm0922@catholic.ac.kr>
2023-08-12 14:47:16 +0900

    Feat: api 수정

--- src/main/java/LikeLion/UnderTheCBackend/controller/LoginController.java
+++ src/main/java/LikeLion/UnderTheCBackend/controller/LoginController.java
@@ -1,5 +1,59 @@
 package LikeLion.UnderTheCBackend.controller;
 
-public class LoginController {
+import LikeLion.UnderTheCBackend.entity.User;
+import LikeLion.UnderTheCBackend.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@Tag(name = "Login/Logout", description = "로그인/로그아웃 API")
+public class LoginController {
+    private UserRepository userRepository;
+
+    LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인", description = "로그인 API", responses = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공")
+    })
+    public String login(HttpServletRequest request, @RequestParam("email") String email) {
+        if (request.getSession(false) != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이미 로그인 되어 있습니다.");
+        }
+
+        Optional<User> result = Optional.ofNullable(this.userRepository.findByEmail(email));
+        User loginUser = result.orElse(null);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("loginUser", loginUser);
+
+        return "sucess";
+    }
+
+    @GetMapping("/logout")
+    @Operation(summary = "로그아웃", description = "로그아웃 API", responses = {
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공")
+    })
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인 되어 있지 않습니다.");
+        }
+        User loginUser = (User) session.getAttribute("loginUser");
+        session.invalidate();
+        return "success";
+    }
 }
--- src/main/java/LikeLion/UnderTheCBackend/controller/UserController.java
+++ src/main/java/LikeLion/UnderTheCBackend/controller/UserController.java
@@ -4,9 +4,12 @@ import LikeLion.UnderTheCBackend.dto.AddUser;
 import LikeLion.UnderTheCBackend.dto.UpdateUser;
 import LikeLion.UnderTheCBackend.entity.User;
 import LikeLion.UnderTheCBackend.repository.UserRepository;
+import LikeLion.UnderTheCBackend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,11 +21,13 @@ import java.util.Optional;
 @RequestMapping("/api/vi/user")
 public class UserController {
     private final UserRepository userRepository;
-    UserController(UserRepository userRepository) {
+    private final UserService userService;
+    UserController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
-    @GetMapping("/id")
+    @GetMapping("/email")
     @Operation(summary = "유저 정보 보기", description = "user 테이블에 지정된 이메일로 유저 정보 반환", responses = {
             @ApiResponse(responseCode = "200", description = "조회 성공")
     })
@@ -32,108 +37,116 @@ public class UserController {
             return user.get();
         }
         else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 유저입니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다.");
         }
     }
 
-    @GetMapping("/id/me")
+    @GetMapping("/email/me")
     @Operation(summary = "로그인 되어 있는 유저 정보 보기", description = "로그인 되어 있는 유저 정보 반환", responses = {
             @ApiResponse(responseCode = "200", description = "조회 성공")
     })
-    public User findMineById(@RequestParam("email") String email) {
-        // 로그인 구현 후, 로그인 되어 있는 이메일의 계정 정보 조회할 예정
+    public User findMineByEmail(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인 되어 있지 않습니다.");
+        }
+
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        String email = loginUser.getEmail();
+
         Optional<User> user = Optional.ofNullable(userRepository.findByEmail(email));
         if (user.isPresent()) {
             return user.get();
         }
         else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 유저입니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다.");
+        }
+    }
+
+    private void isUserNotExist(String email) {
+        Optional<User> existingUser = Optional.ofNullable(userRepository.findByEmail(email));
+        if (existingUser.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "회원가입 실패. 중복회원입니다.");
         }
     }
 
     @PostMapping("/add")
-    @Operation(summary = "유저 추가", description = "user 테이블에 지정된 이메일로 유저 추가", responses = {
+    @Operation(summary = "유저 추가", description = "user 테이블에 유저 추가", responses = {
             @ApiResponse(responseCode = "200", description = "회원가입 완료")
     })
-    public User addById(@RequestBody AddUser json, @RequestParam("email") String email, @RequestParam("photo") String photo) {
-        // 이메일과 프로필 사진은 카카오에서 받아올 예정
+    public String addUser(@RequestBody AddUser json, @RequestParam("email") String email, @RequestParam("profile") String profile) {
         String name = json.getName();
-        String call = json.getCall();
         String nickname = json.getNickname();
+        String phone = json.getPhone();
         String address = json.getAddress();
         String detailAddress = json.getAddress();
         String role = json.getRole();
         String certificate = json.getCertificate();
 
-        User user = new User();
+        isUserNotExist(email);
 
-        if (certificate != null && !certificate.isEmpty()) {
-            user.setName(name);
-            user.setNickname(nickname);
-            user.setCall(call);
-            user.setEmail(email);
-            user.setAddress(address);
-            user.setDetailAddress(detailAddress);
-            user.setRole(role);
-            user.setPhoto(photo);
-            user.setCertificate(certificate);
-        }
-        else {
-            user.setName(name);
-            user.setNickname(nickname);
-            user.setCall(call);
-            user.setEmail(email);
-            user.setAddress(address);
-            user.setDetailAddress(detailAddress);
-            user.setRole(role);
-            user.setPhoto(photo);
-        }
-        userRepository.save(user);
-        return user;
+        userService.createUser(name, nickname, phone, email, address, detailAddress, role, profile, certificate);
+
+        return "success";
     }
 
     @PatchMapping("/update")
     @Operation(summary = "유저 정보 수정", description = "user 테이블에 지정된 이메일로 유저 정보 수정", responses = {
             @ApiResponse(responseCode = "200", description = "수정 완료")
     })
-    public User updateById(@RequestParam("email") String userEmail, @RequestBody UpdateUser json) {
-        // 로그인 구현 후, 로그인 되어 있는 이메일의 계정 정보 수정할 예정
-        Optional<User> beforeUser = Optional.ofNullable(userRepository.findByEmail(userEmail));
+    public User updateByEmail(HttpServletRequest request, @RequestBody UpdateUser json) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "로그인 되어 있지 않습니다.");
+        }
+
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        String email = loginUser.getEmail();
+
+        Optional<User> beforeUser = Optional.ofNullable(userRepository.findByEmail(email));
 
         String name = json.getName();
         String nickname = json.getNickname();
-        String call = json.getCall();
-        String email = userEmail;
+        String phone = json.getPhone();
         String address = json.getAddress();
         String detailAddress = json.getDetailAddress();
-        String photo = json.getPhoto();
+        String profile = json.getProfile();
         String certificate = json.getCertificate();
 
         User afterUser = beforeUser.get();
-        afterUser.setName(name);
-        afterUser.setNickname(nickname);
-        afterUser.setCall(call);
-        afterUser.setAddress(address);
-        afterUser.setDetailAddress(detailAddress);
-        afterUser.setEmail(photo);
-        afterUser.setEmail(certificate);
-        userRepository.save(afterUser);
+
+        if(beforeUser.isPresent()) {
+            afterUser.setName(name);
+            afterUser.setNickname(nickname);
+            afterUser.setPhone(phone);
+            afterUser.setAddress(address);
+            afterUser.setDetailAddress(detailAddress);
+            afterUser.setEmail(profile);
+            afterUser.setEmail(certificate);
+
+            userRepository.save(afterUser);
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다.");
+        }
         return afterUser;
     }
 
-    @DeleteMapping("/delete")
-    @Operation(summary = "유저 삭제", description = "user 테이블에 지정된 이메일로 유저 삭제", responses = {
-            @ApiResponse(responseCode = "200", description = "회원탈퇴 완료")
-    })
-    public User deleteById(@RequestParam("email") String email) {
-        // 로그인 구현 후, 로그인 되어 있을 경우, 안 되어 있을 경우 나누어서 구현할 예정
-        Optional<User> user = Optional.ofNullable(userRepository.findByEmail(email));
-
-        if (user.isPresent()) {
-            userRepository.deleteByEmail(email);
-            return user.get();
-        } else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 계정입니다.");
-        }
-    }
+//    @DeleteMapping("/delete")
+//    @Operation(summary = "유저 삭제", description = "user 테이블에 지정된 이메일로 유저 삭제", responses = {
+//            @ApiResponse(responseCode = "200", description = "회원탈퇴 완료")
+//    })
+//    public User deleteByEmail(@RequestParam("email") String email) {
+//        // 로그인 구현 후, 로그인 되어 있을 경우, 안 되어 있을 경우 나누어서 구현할 예정
+//        Optional<User> user = Optional.ofNullable(userRepository.findByEmail(email));
+//
+//        if (user.isPresent()) {
+//            userRepository.deleteByEmail(email);
+//            return user.get();
+//        } else {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자입니다.");
+//        }
+//    }
 }
