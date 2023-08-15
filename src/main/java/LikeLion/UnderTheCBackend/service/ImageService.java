28f3c6ee96a6efa0458c0ce10faa8b8de8ba648a
feedbaek <nicek789@naver.com>
2023-08-15 16:40:36 +0900

    Fix: image download 경로 문제 수정

--- src/main/java/LikeLion/UnderTheCBackend/service/ImageService.java
+++ src/main/java/LikeLion/UnderTheCBackend/service/ImageService.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,7 +14,9 @@ import java.io.InputStream;
 public class ImageService {
 
     public ResponseEntity<byte[]> downloadImage(String imageName) throws IOException {
-        ClassPathResource imageFile = new ClassPathResource("images" + imageName);
+        ClassPathResource imageFile = new ClassPathResource("images/" + imageName);
+        if (!imageFile.exists())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "이미지를 찾을 수 없습니다.");
         InputStream imageStream = imageFile.getInputStream();
 
         byte[] imageByteArray = imageStream.readAllBytes();
